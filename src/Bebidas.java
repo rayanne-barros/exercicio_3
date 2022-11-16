e9bfdaced16465ef3767619ed8350d123485efba
rayanne-barros <rayanneb126@gmail.comom>
2022-11-16 15:06:24 -0300

    Adicionando o preço das bebidas

--- src/Bebidas.java
+++ src/Bebidas.java
@@ -12,13 +12,13 @@ public class Bebidas {
         System.out.println("Escolha uma das opções de bebidas");
         Integer opcao = sc.nextInt();
         switch (opcao) {
-            case 1 -> System.out.println("O produto escolhido foi: Coca-Cola - R$ 5");
-            case 2 -> System.out.println("O produto escolhido foi: Coca-Cola 0 - R$ 4.50");
-            case 3 -> System.out.println("O produto escolhido foi: Pepsi - R$ 4.40");
-            case 4 -> System.out.println("O produto escolhido foi: Guaraná Antarctica - R$ 3.50");
-            case 5 -> System.out.println("O produto escolhido foi: Fanta Laranja - R$ 4.23");
-            case 6 -> System.out.println("O produto escolhido foi: Água - R$ 2.50");
-            default -> System.out.println("Produto não encontrado");
+            case 1 -> System.out.println("O produto escolhido foi: Coca-Cola. O preço é: R$ 5");
+            case 2 -> System.out.println("O produto escolhido foi: Coca-Cola 0. O preço é: R$ 4.50");
+            case 3 -> System.out.println("O produto escolhido foi: Pepsi. O preço é:R$ 4.40");
+            case 4 -> System.out.println("O produto escolhido foi: Guaraná Antarctica. O preço é: R$ 3.50");
+            case 5 -> System.out.println("O produto escolhido foi: Fanta Laranja. O preço é: R$ 4.23");
+            case 6 -> System.out.println("O produto escolhido foi: Água. O preço é: R$ 2.50");
+            default -> System.out.println("Escolha uma das opções de bebidas do menu");
         }
     }
 }
--- src/Bebidas2.java
+++ src/Bebidas2.java
@@ -12,19 +12,19 @@ public class Bebidas2 {
         System.out.println("Escolha uma das opções de bebidas");
         Integer opcao = sc.nextInt();
         if (opcao == 1) {
-            System.out.println("O produto escolhido foi: Coca-Cola - R$ 5");
+            System.out.println("O produto escolhido foi: Coca-Cola. O preço é: R$ 5");
         } else if(opcao == 2) {
-            System.out.println("O produto escolhido foi: Coca-Cola 0 - R$ 4.50");
+            System.out.println("O produto escolhido foi: Coca-Cola 0. O preço é: R$ 4.50");
         } else if (opcao == 3) {
-            System.out.println("O produto escolhido foi: Pepsi - R$ 4.40");
+            System.out.println("O produto escolhido foi: Pepsi. O preço é: R$ 4.40");
         } else if (opcao == 4) {
-            System.out.println("O produto escolhido foi: Guaraná Antarctica - R$ 3.50");
+            System.out.println("O produto escolhido foi: Guaraná Antarctica. O preço é: R$ 3.50");
         } else if (opcao == 5) {
-            System.out.println("O produto escolhido foi: Fanta Laranja - R$ 4.23");
+            System.out.println("O produto escolhido foi: Fanta Laranja. O preço é: R$ 4.23");
         } else if (opcao == 6) {
-            System.out.println("O produto escolhido foi: Água - R$ 2.50");
+            System.out.println("O produto escolhido foi: Água. O preço é: R$ 2.50");
         } else {
-            System.out.println("Produto não encontrado");
+            System.out.println("Escolha uma das opções de bebidas do menu");
         }
     }
 }
